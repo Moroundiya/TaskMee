38181e7bc8179be026a9086887fb7fad6a7388e9
MOROUNDIYA <moroundiya@outlook.com>
2024-04-27 00:28:26 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,9 @@ function App() {
   }, [value, priority, task, getDate, initialtime, deadlinetime])
 
 
-  function addTaskModal() {
+  function addTaskModal(e) {
+
+    e.preventDefault();
 
     setTask(task => [
       ...task,
@@ -65,40 +67,48 @@ function App() {
   return (
     <div className='w-full h-full bg-[#eee] relative'>
       <div className={`absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,.4)] ${showModal} justify-center items-center px-4 sm:px-0 transition-all duration-300 ease-in-out`}>
+
         <div className='bg-white h-auto w-full sm:w-[600px] rounded-lg px-4 py-8'>
           <div className='flex justify-between items-center mb-5'>
             <p className='text-lg sm:text-xl'>Add Task</p>
             <Icon icon="ci:close-sm" className='text-3xl sm:text-4xl cursor-pointer -mr-2' onClick={() => setshowModal('hidden')} />
           </div>
-          <input type="text" name="" id="" className='w-full h-[45px] border-[#333333a9] border mt-5 outline-none px-2 rounded'
-            placeholder='Task Name' value={value} onInput={(e) => { setValue(e.target.value); }} maxLength={17} required />
-          <p className='mt-5 text-[#242121] text-md'>Priority</p>
-          <div className='mt-3 text-[14.8px]'>
-            <button className={high ? 'py-1 px-4 border bg-red-700 text-white border-red-700 mr-4 rounded transition-all ease-in-out duration-200 hover:bg-red-700 hover:text-whites' : 'py-1 px-4 border border-red-700 mr-4 rounded transition-all ease-in-out duration-200 text-red-700 hover:bg-red-700 hover:text-white'} onClick={() => setPriority('high')}>High</button>
-            <button className={medium ? 'py-1 px-4 border border-orange-400 mr-4 bg-orange-400 text-white rounded transition-all ease-in-out duration-200 hover:bg-orange-400 hover:text-white' : 'py-1 px-4 border border-orange-400 mr-4 rounded transition-all ease-in-out duration-200 text-orange-400 hover:bg-orange-400 hover:text-white'} onClick={() => setPriority('medium')}>Medium</button>
-            <button className={low ? 'py-1 px-4 border border-gray-700 bg-gray-700 mr-4 rounded transition-all ease-in-out duration-200 text-white hover:bg-gray-700 hover:text-white' : 'py-1 px-4 border border-gray-700 mr-4 rounded transition-all ease-in-out duration-200 text-gray-700 hover:bg-gray-700 hover:text-white'} onClick={() => setPriority('low')}>Low</button>
-          </div>
 
-          <div className='mt-7 flex items-center'>
-            <p>Date: </p>
-            <input type="date" className='outline-none p-1 ms-2' value={getDate} onChange={(e) => setgetDate(e.target.value)} placeholder='Select Date' required />
-          </div>
+          <form action="" onSubmit={addTaskModal}>
+            <input type="text" name="" id="" className='w-full h-[45px] border-[#333333a9] border mt-5 outline-none px-2 rounded'
+              placeholder='Task Name' value={value} onInput={(e) => { setValue(e.target.value); }} maxLength={17} required />
+            <p className='mt-5 text-[#242121] text-md'>Priority</p>
+            <div className='mt-3 text-[14.8px]'>
+              <button className={high ? 'py-1 px-4 border bg-red-700 text-white border-red-700 mr-4 rounded transition-all ease-in-out duration-200 hover:bg-red-700 hover:text-whites' : 'py-1 px-4 border border-red-700 mr-4 rounded transition-all ease-in-out duration-200 text-red-700 hover:bg-red-700 hover:text-white'} onClick={() => setPriority('high')}>High</button>
+              <button className={medium ? 'py-1 px-4 border border-orange-400 mr-4 bg-orange-400 text-white rounded transition-all ease-in-out duration-200 hover:bg-orange-400 hover:text-white' : 'py-1 px-4 border border-orange-400 mr-4 rounded transition-all ease-in-out duration-200 text-orange-400 hover:bg-orange-400 hover:text-white'} onClick={() => setPriority('medium')}>Medium</button>
+              <button className={low ? 'py-1 px-4 border border-gray-700 bg-gray-700 mr-4 rounded transition-all ease-in-out duration-200 text-white hover:bg-gray-700 hover:text-white' : 'py-1 px-4 border border-gray-700 mr-4 rounded transition-all ease-in-out duration-200 text-gray-700 hover:bg-gray-700 hover:text-white'} onClick={() => setPriority('low')}>Low</button>
+            </div>
 
-          <div className='mt-7 flex items-center'>
-            <p className='me-2'>Duration:</p>
-            <input type="text" className='border w-[80px] rounded border-[#333] outline-none py-1 px-2 text-[15px]' placeholder='12:00am' value={initialtime} onChange={(e) => setinitialtime(e.target.value)} required />
-            <span className='mx-3'> - </span>
-            <input type="text" className='border w-[80px] rounded border-[#333] outline-none py-1 px-2 text-[15px]' placeholder='2:00am' value={deadlinetime} onChange={(e) => setdeadlinetime(e.target.value)} required />
-          </div>
+            <div className='mt-7 flex items-center'>
+              <p>Date: </p>
+              <input type="date" className='outline-none p-1 ms-2' value={getDate} onChange={(e) => setgetDate(e.target.value)} placeholder='Select Date' required />
+            </div>
 
-          <div className='mt-16 flex justify-end'>
-            <button className='flex items-center bg-blue-900 ps-3 pe-4 py-[.45rem] rounded text-white'>
-              <Icon icon="fa6-solid:plus" className='mr-2 text-lg' />
-              <span onClick={addTaskModal}>Add Task Now</span>
-            </button>
-          </div>
+            <div className='mt-7 flex items-center'>
+              <p className='me-2'>Duration:</p>
+              <input type="text" className='border w-[80px] rounded border-[#333] outline-none py-1 px-2 text-[15px]' placeholder='12:00am' value={initialtime} onChange={(e) => setinitialtime(e.target.value)} required />
+              <span className='mx-3'> - </span>
+              <input type="text" className='border w-[80px] rounded border-[#333] outline-none py-1 px-2 text-[15px]' placeholder='2:00am' value={deadlinetime} onChange={(e) => setdeadlinetime(e.target.value)} required />
+            </div>
+
+            <div className='mt-16 flex justify-end'>
+              <button className='flex items-center bg-blue-900 ps-3 pe-4 py-[.45rem] rounded text-white' type='submit'>
+                <Icon icon="fa6-solid:plus" className='mr-2 text-lg' />
+                <span>Add Task Now</span>
+              </button>
+            </div>
+
+          </form>
         </div>
       </div>
+
+
+
       <div className='sm:w-[600px] mx-auto pt-8 px-3 xl:pt-12'>
         <div className='flex justify-between items-center'>
           <h1 className='text-2xl font-paci'>TaskMee</h1>
@@ -110,7 +120,7 @@ function App() {
 
 
         <div className='mt-10 border-[#5957575f] border-t overflow-y-auto h-[550px] w-full'>
-          {task.map((task) => <AddTask newValue={task.taskName} getDate={task.taskDate} initialtime={task.durationFrom} deadlinetime={task.durationTo} key={task.newValue} />)}
+          {task.map((task) => <AddTask newValue={task.taskName} getDate={task.taskDate} initialtime={task.durationFrom} deadlinetime={task.durationTo} key={task.newValue} priority={task.urgency} />)}
         </div>
 
         <footer className='text-center pt-5'>
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Icon } from '@iconify/react';
 
 
-export const AddTask = ({ newValue, getDate, initialtime, deadlinetime }) => {
+export const AddTask = ({ newValue, getDate, initialtime, deadlinetime, priority }) => {
+
+
+    const [complete, setComplete] = useState();
+    const [newcomplete, setnewcomplete] = useState();
+
+    useEffect(() => {
+        setnewcomplete(complete)
+    }, [complete])
+
+    const taskComplete = event => {
+        if (event.target.checked) {
+            setComplete(true)
+        } else {
+            setComplete(false)
+        }
+    }
+
+    const editTask = () => {
+        alert('Hi')
+    }
+
+
+
+
     return (
-        <div className='flex items-center justify-bektween py-2 border-[#5957575f] border-b h-[75px] xl:h-[70px] w-full'>
-            <div className='px-3 border-l-4 rounded-sm border-yellow-500 flex items-center h-full py-4 w-[58%] '>
-                <input type="checkbox" className='cursor-pointer w-[17px] h-[17px]' />
+        <div className='flex items-center py-2 border-[#5957575f] border-b h-[75px] xl:h-[70px] w-full'>
+            <div className={newcomplete ? 'px-3 border-l-4 border-green-500 rounded-sm flex items-center h-full py-4 w-[58%]' : `px-3 border-l-4 ${priority} rounded-sm flex items-center h-full py-4 w-[58%]`}>
+                <input type="checkbox" className='cursor-pointer w-[17px] h-[17px]' onChange={taskComplete} />
                 <div className='ml-4 sm:ml-6'>
-                    <h1 className='text-xl '>{newValue}</h1>
-                    <p className='text-[12px] leading-tight'>{getDate}</p>
-                    <p className='text-[11px] leading-tight'>{initialtime} - {deadlinetime}</p>
+                    <h1 className={newcomplete ? 'text-xl line-through' : 'text-xl'}>{newValue}</h1>
+                    <p className={newcomplete ? 'text-[12px] leading-tight line-through' : 'text-[12px] leading-tight'}>{getDate}</p>
+                    <p className={newcomplete ? 'text-[11px] leading-tight line-through' : 'text-[11px] leading-tight'}>{initialtime} - {deadlinetime}</p>
                 </div>
             </div>
             <div className='w-[18%] sm:w-[15%]'>
-            <p className='text-white bg-yellow-500 px-2 py-1 rounded-md text-[12.8px] text-center'>Pending</p>
+                <p className={newcomplete ? 'text-white bg-green-500 px-2 py-1 rounded-md text-[12.8px] text-center' : 'text-white bg-yellow-500 px-2 py-1 rounded-md text-[12.8px] text-center'}>{newcomplete ? 'Completed' : 'Pending'}</p>
             </div>
-            <div className='flex text-xl sm:text-2xl space-x-3 sm:space-x-4 pe-1 xl:pe-2 justify-end w-[24%] sm:w-[37%]'>
-                <Icon icon="bx:edit" className='cursor-pointer' />
-                <Icon icon="ion:trash" className='cursor-pointer' />
+            <div className={newcomplete ? 'flex text-xl sm:text-2xl space-x-3 sm:space-x-4 pe-1 xl:pe-2 justify-end w-[24%] sm:w-[37%] text-gray-600 pointer-events-none' : 'flex text-xl sm:text-2xl space-x-3 sm:space-x-4 pe-1 xl:pe-2 justify-end w-[24%] sm:w-[37%]'}>
+                <Icon icon="bx:edit" className='cursor-pointer' onClick={editTask} />
+                <Icon icon="ion:trash" className='cursor-pointer' onClick={editTask} />
             </div>
         </div>
     )
