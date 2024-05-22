c4dec9c2c67a6436e9b7beca416c80540c3264a2
MOROUNDIYA <moroundiya@outlook.com>
2024-05-22 02:21:19 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -357,14 +357,14 @@ function App() {
           <h1 className='text-2xl font-paci dark:text-[#E2E8F0]'>TaskMee</h1>
           <div className='flex flex-col items-center'>
             <div>
-              <input type="checkbox" class="checkbox" id="checkbox" />
+              <input type="checkbox" class="checkbox" id="checkbox" checked={themeValue} onChange={changeThemeMode} />
               <label for="checkbox" class="checkbox-label">
                 <i class="fas fa-moon"></i>
                 <i class="fas fa-sun"></i>
                 <span class="ball"></span>
               </label>
             </div>
-            <span className='mt-1 text-sm'>Light Mode</span>
+            <span className='mt-1 text-sm dark:text-[#E2E8F0]'>{themeValue ? 'Dark Mode' : 'Light Mode'}</span>
           </div>
           <button className='bg-blue-900 px-2 py-[7px] sm:py-2 sm:px-4 cursor-pointer text-white rounded font-[acme] flex items-center' onClick={() => setshowModal('flex')}>
             <Icon icon="fa6-solid:plus" className='mr-2 sm:text-lg' />
